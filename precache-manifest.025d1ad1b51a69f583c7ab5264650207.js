a9ac347113ef0fe8972a50916e3d22d3c30b1a30
smoot <smoothtacomann@gmail.com>
2019-01-18 23:15:19 -0500

    Updates

---
+++ precache-manifest.025d1ad1b51a69f583c7ab5264650207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "b7f4e01c72940de7e36b",
+    "url": "/Clicky-Game/static/js/main.b7f4e01c.chunk.js"
+  },
+  {
+    "revision": "1d578b0681e3665c2b8b",
+    "url": "/Clicky-Game/static/js/1.1d578b06.chunk.js"
+  },
+  {
+    "revision": "b7f4e01c72940de7e36b",
+    "url": "/Clicky-Game/static/css/main.63fc555b.chunk.css"
+  },
+  {
+    "revision": "4cb256a1023ccb255d49226bd622a9b8",
+    "url": "/Clicky-Game/index.html"
+  }
+];
